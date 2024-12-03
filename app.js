69b7942b5e85a84140ef93aee904b52b1095d430
Mohan-Naudiyal <naudiyalmohan129@gmail.com>
2024-12-03 20:17:34 +0530

    add new files

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express") ;
 const app = express() ;
 const mongoose = require('mongoose');
 const Listing = require("./models/listing.js") ;
+const path = require("path") ;
+const methodOverride = require("method-override");
 
 
 main().then(() => {
@@ -13,22 +15,86 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
+app.set("view engine" , "ejs") ;
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method")) ;
 
-app.get("/testListing" ,async (req , res) => {
-  let sampleListing = new Listing({
-    title : "My New Villa" ,
-    description : "By the beach" ,
-    // image : "https://images.pexels.com/photos/1213447/pexels-photo-1213447.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    price : 7000 , 
-    location : "Mumbai" ,
-    country : "India" ,
-  })
-  await sampleListing.save().then((res) => {
-    console.log(res) ;
-  }).catch(err => console.log(err)) ;
-  res.send("Connection Succesfully .")
+// index route
+
+app.get("/listings" , async(req ,res) => {
+  const allStrings = await Listing.find({});
+  res.render("index.ejs" ,{allStrings});
+  
+})
+
+// new route
+
+app.get("/listings/new" ,(req ,res) => {
+  res.render("new.ejs") ;
+})
+
+//create route
+app.post("/listings" ,async(req ,res) => {
+  let newListing = new Listing(req.body.listing) ;
+  console.log(newListing)
+  await newListing.save();
+  res.redirect("/listings" );
+})
+
+//edit route
+
+app.get("/listings/:id/edit" , async(req ,res) => {
+  const {id} = req.params ;
+  let lists = await Listing.findById(id)
+  res.render("edit.ejs" ,{lists})
 
 })
+
+// update Msg route
+
+app.put("/listings/:id" , async(req , res) => {
+  const {id} = req.params ;
+  await Listing.findByIdAndUpdate(id , {...req.body.listing})
+  res.redirect(`/listings/${id}`) ;
+})
+
+// delete route
+
+app.delete("/listings/:id" ,async (req ,res) => {
+  let {id} = req.params ;
+  await  Listing.findByIdAndDelete(id ,{...req.body.listing}) ;
+  res.redirect("/listings") ;
+})
+
+
+// particular route
+
+app.get("/listings/:id" , async(req ,res) => {
+  let {id} = req.params ;
+  const lists = await Listing.findById(id);
+  res.render("show.ejs" , {lists}) ;
+  
+})
+
+
+
+
+
+// app.get("/testListing" ,async (req , res) => {
+//   let sampleListing = new Listing({
+//     title : "My New Villa" ,
+//     description : "By the beach" ,
+//     // image : "https://images.pexels.com/photos/1213447/pexels-photo-1213447.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//     price : 7000 , 
+//     location : "Mumbai" ,
+//     country : "India" ,
+//   })
+//   await sampleListing.save().then((res) => {
+//     console.log(res) ;
+//   }).catch(err => console.log(err)) ;
+//   res.send("Connection Succesfully .") ;
+// })
 
 app.listen(3000 , (req , res) => {
   console.log("I am listening .")
